d56c84a232f896e369989fd685d47bc018764ac4
derryn-knife <derryn.knife@gmail.com>
2022-03-06 14:16:05 +1000

    feat: Added browser router
    index.js was modified to include BrowserRouter to allow for
    multiple routes, pages, to be used by the "single page app"
    This allows for a normal user experience regarding the back and
    forward buttons.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import Search from './Search'
 import './index.css'
+import { Route, Routes } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="search" element={<Search />} />
+        </Routes>
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
