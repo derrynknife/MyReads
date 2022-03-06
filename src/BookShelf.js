7f2cb1a87890c6f006529e54369b37c6103cc596
derryn-knife <derryn.knife@gmail.com>
2022-03-06 21:07:15 +1000

    feat: Added new logic to BooksGrid to check for undefined books. Added destructuring to BookShelf to make it look much tidier.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,17 +3,22 @@ import Book from './Book'
 
 export class BooksGrid extends Component {
     render() {
-        const { bookIDs, books } = this.props
+        const { bookIDs, books, shelves } = this.props;
         return (
             <ol className="books-grid">
-                {bookIDs.map((book) => (
+                {bookIDs.map((bookID) => {
+                    let shelf = shelves[bookID]
+                    if (!shelf) {
+                        shelf = "none"
+                    }
+                    return (
                     <Book 
-                        key={book}
-                        book={books[book]}
-                        shelf={books[book].shelf}
+                        key={bookID}
+                        book={books[bookID]}
+                        shelf={shelf}
                         onShelfUpdate={this.props.onShelfUpdate}
                     />
-                ))}
+                )})}
             </ol>
         )
     }
@@ -21,14 +26,22 @@ export class BooksGrid extends Component {
 
 class BookShelf extends Component {
     render() {
+        const { books, onShelfUpdate, bookIDs } = this.props
+        const { title } = this.props.shelf
+        let shelves = {};
+        for (const bookID of bookIDs) {
+            shelves[bookID] = books[bookID].shelf
+        }
+
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelf.title}</h2>
+                <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <BooksGrid
-                        bookIDs={this.props.bookIDs}
-                        books={this.props.books}
-                        onShelfUpdate={this.props.onShelfUpdate}
+                        bookIDs={bookIDs}
+                        books={books}
+                        shelves={shelves}
+                        onShelfUpdate={onShelfUpdate}
                     />
                 </div>
             </div>
